fa486534b406eb9bd101321dd747b48224d649e1
udhaybegyall <38879140+udhaybegyall@users.noreply.github.com>
2020-11-22 17:54:01 +0530

    Update PDFconvertor.py
    Added Command Line support .
    Code optimization .
    and some minor changes.

--- PDFConverter.py
+++ PDFConverter.py
@@ -4,41 +4,60 @@
 ##############################################
 
 import os
+import argparse
 from time import strftime
 from win32com import client
 
+parser = argparse.ArgumentParser(
+	description = "Convert doc/docx to pdf"
+)
+
+# Argument --path for cli use
+parser.add_argument(
+	"--path",
+	type = str,
+	default = "."
+	help = "Directory path were document files are"
+)
+
+args = parser.parse_args()
+path = args.path
+
+dir_content = os.listdir(path)
+path_dir_content = [os.path.join(path, doc) for doc in dir_content]
+docs = [doc for doc in path_dir_content if os.path.isfile(doc)]
+
 # Counts the number of files in the directory that can be converted
-def n_files(directory):
-    total = 0
-    for file in os.listdir(directory):
-        if (file.endswith('.doc') or file.endswith('.docx') or file.endswith('.tmd')):
-            total += 1
-    return total
+def n_files(path):
+    return len(docs)
 
 # Creates a new directory within current directory called PDFs
-def createFolder(directory):
-    if not os.path.exists(directory + '\\PDFs'):
-        os.makedirs(directory + '\\PDFs')
+def createFolder(path):
+	if not os.path.exists(path + '\\PDFs'):
+		try:
+			os.mkdir(path + '\\PDFs')
+			print("\nfolder created.")
+		except FileExistsError as er:
+			print(f"\nFolder already exists at {path}.. {er}")
+    
 		
 if __name__ == "__main__":
     print('\nPlease note that this will overwrite any existing PDF files')
     print('For best results, close Microsoft Word before proceeding')
     input('Press enter to continue.')
 	
-    directory = os.getcwd()
-	
-    if n_files(directory) == 0:
-        print('There are no files to convert')
+    if n_files(path) == 0:
+        print('\nThere are no files to convert')
         exit()
 		
-    createFolder(directory)
+    createFolder(path)
 	
-    print('Starting conversion... \n')
+    print(f"\nStarting conversion of {len(docs)} docs... \n")
 	
     # Opens each file with Microsoft Word and saves as a PDF
     try:
         word = client.DispatchEx('Word.Application')
-        for file in os.listdir(directory):
+        for file in dir_content:
             if (file.endswith('.doc') or file.endswith('.docx') or file.endswith('.tmd')):
                 ending = ""
                 if file.endswith('.doc'):
@@ -48,8 +67,8 @@ if __name__ == "__main__":
                 if file.endswith('.tmd'):
                     ending = '.tmd'
                 new_name = file.replace(ending,r".pdf")
-                in_file = os.path.abspath(directory + '\\' + file)
-                new_file = os.path.abspath(directory + '\\PDFs' + '\\' + new_name)
+                in_file = os.path.abspath(path + '\\' + file)
+                new_file = os.path.abspath(path + '\\PDFs' + '\\' + new_name)
                 doc = word.Documents.Open(in_file)
                 print(new_name)
                 doc.SaveAs(new_file,FileFormat = 17)
