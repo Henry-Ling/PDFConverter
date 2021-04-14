62b3c0f4e4f72f5ec118761fbb127836409714b1
Thiago Santos <61919603+thiagonds65@users.noreply.github.com>
2021-04-14 18:48:32 -0300

    Add files via upload
    Some lines was added in PDFConverter to allow LibreOffice users to make convert from .doc and .docx to .pdf. It works on Linux (Ubuntu 20.04 distro)

--- PDFConverter.py
+++ PDFConverter.py
@@ -5,7 +5,7 @@
 
 import os
 from time import strftime
-from win32com import client
+from sys import platform, exc_info
 
 # Counts the number of files in the directory that can be converted
 def n_files(directory):
@@ -17,9 +17,36 @@ def n_files(directory):
 
 # Creates a new directory within current directory called PDFs
 def createFolder(directory):
-    if not os.path.exists(directory + '\\PDFs'):
-        os.makedirs(directory + '\\PDFs')
-		
+    if not os.path.exists(directory + '/PDFs'):
+        os.makedirs(directory + '/PDFs')
+
+def doc2pdf_libreoffice(doc, ending, newdic):
+    """
+    convert a doc/docx document to pdf format (linux only, requires libreoffice)
+    :param doc: path to document
+    """
+    cmd = f"lowriter --convert-to pdf:writer_pdf_Export '{doc}'"
+    os.system(cmd)
+    new_file = doc.replace(ending, r".pdf")
+    if platform =='linux':
+        cmdmove = f"mv '{new_file}' '{newdic}'"
+
+    elif platform == 'win32':
+        new_file = new_file.replace("/", "\\")
+        cmdmove = f"move '{new_file}' '{newdic}'"
+    
+    os.system(cmdmove)
+    print(new_file)
+
+def is_tool(name):
+    """Check whether `name` is on PATH and marked as executable."""
+
+    # from whichcraft import which
+    from shutil import which
+
+    return which(name) is not None
+
+
 if __name__ == "__main__":
     print('\nPlease note that this will overwrite any existing PDF files')
     print('For best results, close Microsoft Word before proceeding')
@@ -34,10 +61,12 @@ if __name__ == "__main__":
     createFolder(directory)
 	
     print('Starting conversion... \n')
-	
+
     # Opens each file with Microsoft Word and saves as a PDF
     try:
-        word = client.DispatchEx('Word.Application')
+        if(is_tool('libreoffice') == False):
+            from win32com import client
+            word = client.DispatchEx('Word.Application')
         for file in os.listdir(directory):
             if (file.endswith('.doc') or file.endswith('.docx') or file.endswith('.tmd')):
                 ending = ""
@@ -47,16 +76,21 @@ if __name__ == "__main__":
                     ending = '.docx'
                 if file.endswith('.tmd'):
                     ending = '.tmd'
-                new_name = file.replace(ending,r".pdf")
-                in_file = os.path.abspath(directory + '\\' + file)
-                new_file = os.path.abspath(directory + '\\PDFs' + '\\' + new_name)
-                doc = word.Documents.Open(in_file)
-                print(new_name)
-                doc.SaveAs(new_file,FileFormat = 17)
-                doc.Close()
-    except :
-        print("Error: Aborting")
-    finally:
-        word.Quit()
+                if is_tool('libreoffice'):
+                    in_file = os.path.abspath(directory + '/' + file)
+                    new_file = os.path.abspath(directory + '/PDFs')
+                    doc2pdf_libreoffice(in_file, ending, new_file)
+
+                if(is_tool('libreoffice') == False):
+                    new_name = file.replace(ending,r".pdf")
+                    in_file = os.path.abspath(directory + '\\' + file)
+                    new_file = os.path.abspath(directory + '\\PDFs' + '\\' + new_name)
+                    doc = word.Documents.Open(in_file)
+                    print(new_name)
+                    doc.SaveAs(new_file,FileFormat = 17)
+                    doc.Close()
+                
+    except Exception as e:
+        print(e)
 
     print('\nConversion finished at ' + strftime("%H:%M:%S"))
